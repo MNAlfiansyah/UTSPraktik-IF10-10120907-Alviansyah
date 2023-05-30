50c54f1a1c7ee1be8afe090a9929c5af385c238f
MNAlfiansyah <MNAlfiansyah33@gmail.com>
2023-05-30 20:21:53 +0700

    10120907

---
+++ app/src/main/java/com/example/utspraktik_if10_10120907_alviansyah/MainActivity.kt
@@ -0,0 +1,16 @@
+// Tanggal Pengerjaan : 30 May 2023
+// Nim : 10120907
+// Nama : Muhammad Nur Alviansyah
+// Kelas : IF10K
+
+package com.example.utspraktik_if10_10120907_alviansyah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
